06d7909c7d2da0b33c2a5682970263412ecff9cd
sandiprumba <sandiprumba.lama@gmail.com>
2024-07-01 11:46:05 +0800

    Deployement

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 import cookieParser from "cookie-parser";
 import { app, server } from "./socket/Socket.js";
 
@@ -10,6 +11,7 @@ import userRoutes from "./routes/user.routes.js";
 import messageRoute from "./routes/message.routes.js";
 
 const PORT = process.env.PORT || 8000;
+const __dirname = path.resolve();
 
 dotenv.config();
 app.use(express.json());
@@ -18,6 +20,14 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoute);
 app.use("/api/users", userRoutes);
+
+//path.join gives the absolute folder
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+//any other routes
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
   connectMongoDB();
